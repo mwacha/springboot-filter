119412add05c772ff754bfe9e10b928694aee264
marcelo <marcelo.ferreira@contaazul.com>
2022-11-18 16:42:25 -0300

    second commit

--- src/main/java/tk/mwacha/http/UserController.java
+++ src/main/java/tk/mwacha/http/UserController.java
@@ -15,16 +15,7 @@ import java.util.UUID;
 @Slf4j
 public class UserController {
 
-    @GetMapping
-    public List<UserDTO> getAllUsers() {
-        log.info("Fetching all the users");
-        return Arrays.asList(
-                UserDTO.builder().id(UUID.randomUUID().toString()).name("User1").email("user1@mail.com").build(),
-                UserDTO.builder().id(UUID.randomUUID().toString()).name("User2").email("user2@mail.com").build(),
-                UserDTO.builder().id(UUID.randomUUID().toString()).name("User3").email("user3@mail.com").build());
-    }
-
-    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
+    @PostMapping(produces = MediaType.APPLICATION_JSON_VALUE)
     public void save(@RequestBody List<UserDTO> users) {
         log.info("save users: {}", users.stream().count());
 
--- src/main/java/tk/mwacha/http/filters/RequestResponseLoggingFilter.java
+++ src/main/java/tk/mwacha/http/filters/RequestResponseLoggingFilter.java
@@ -11,9 +11,10 @@ import java.io.BufferedInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.nio.charset.Charset;
+import java.util.stream.Collectors;
 
 @Component
-@Order(2)
+@Order(1)
 @Slf4j
 public class RequestResponseLoggingFilter implements Filter {
 
@@ -30,39 +31,14 @@ public class RequestResponseLoggingFilter implements Filter {
 
         log.info("Logging Request  {} : {}", req.getMethod(), req.getRequestURI());
 
-        chain.doFilter(request, response);
+        String bodyInStringFormat = request.getReader().lines().collect(Collectors.joining(System.lineSeparator()));
+
+        log.info("Request body: {}", bodyInStringFormat);
 
         log.info("Logging Response :{}", res.getWriter());
 
-        String characterEncoding = req.getCharacterEncoding();
-        Charset charset = Charset.forName(characterEncoding);
-        String bodyInStringFormat = readInputStreamInStringFormat(req.getInputStream(), charset);
-        log.info("Request body: {}", bodyInStringFormat);
 
     }
-
-    private String readInputStreamInStringFormat(InputStream stream, Charset charset) throws IOException {
-        final int MAX_BODY_SIZE = 2048;
-        final StringBuilder bodyStringBuilder = new StringBuilder();
-        if (!stream.markSupported()) {
-            stream = new BufferedInputStream(stream);
-        }
-
-        stream.mark(MAX_BODY_SIZE + 1);
-        final byte[] entity = new byte[MAX_BODY_SIZE + 1];
-        final int bytesRead = stream.read(entity);
-
-        if (bytesRead != -1) {
-            bodyStringBuilder.append(new String(entity, 0, Math.min(bytesRead, MAX_BODY_SIZE), charset));
-            if (bytesRead > MAX_BODY_SIZE) {
-                bodyStringBuilder.append("...");
-            }
-        }
-        stream.reset();
-
-        return bodyStringBuilder.toString();
-    }
-
 
     @Override
     public void destroy() {
